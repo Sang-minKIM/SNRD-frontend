7b76fb60fd9465754352a5cd91dd89d680054648
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2022-12-19 14:44:48 +0900

    hide scrollbar

--- src/routers/Intro.tsx
+++ src/routers/Intro.tsx
@@ -4,6 +4,9 @@ import intro from "../assets/intro.svg";
 const Image = styled.img`
   width: 100vw;
   overflow-y: scroll;
+  ::-webkit-scrollbar {
+    display: none;
+  }
   margin-top: 50px;
 `;
 export function Intro() {
