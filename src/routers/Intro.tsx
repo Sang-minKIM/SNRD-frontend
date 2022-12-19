02e1f959d48057d1242b39f72430f08afbc49221
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2022-12-19 20:56:13 +0900

    redeploy

--- src/routers/Intro.tsx
+++ src/routers/Intro.tsx
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 
-const Image = styled.img`
-  width: 100vw;
-  overflow-y: scroll;
-  ::-webkit-scrollbar {
-    display: none;
-  }
-  margin-top: 50px;
-`;
+// const Image = styled.img`
+//   width: 100vw;
+//   overflow-y: scroll;
+//   ::-webkit-scrollbar {
+//     display: none;
+//   }
+//   margin-top: 50px;
+// `;
 export function Intro() {
   return <>{/* <Image src={intro} /> */}</>;
 }
